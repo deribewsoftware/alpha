56e990b733aaa8bd426b13d2592818ae8ab8b27b
deribewsoftware <deribewsoftware@gmail.com>
2024-02-11 15:31:10 +0300

    updated

--- app.ts
+++ app.ts
@@ -13,7 +13,13 @@ app.use(express.json({limit:"50mb"}))
 app.use(cookieParser())
 
 // cors origin
-app.use(cors())
+app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 //routes
 app.use('/api/v1',userRouter)
